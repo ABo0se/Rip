42852e0229a10fa62f6cdfae36403aa4bc61ce1b
ABoOse <thanawat.65938@mail.kmutt.ac.th>
2020-06-11 00:42:03 +0700

    Background

---
+++ Assets/bg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class bg : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public float xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+        material.mainTextureOffset += offset * Time.deltaTime;
+        //(Bonustime Mechanic)
+    }
+
+}
+
+
---
+++ Assets/girl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girl : MonoBehaviour
+{
+    [SerializeField] KeyCode Jump;
+    [SerializeField] KeyCode Slide;
+    public float jumpSpeed;
+    Rigidbody2D rb;
+    Animator am;
+    int jump;
+
+    //Use this for initialzation
+    void Start()
+    {
+        jump = 0;
+        rb = GetComponent<Rigidbody2D>();
+        am = GetComponent<Animator>();
+    }
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(Jump) && jump < 2)
+        {
+            jump++;
+            am.SetBool("Jump", true);
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        jump = 0;
+        am.SetBool("Jump", false);
+    }
+}
+    
+
+
